e8e28f9c8bd149ed7411b8f00900e02d765fad6a
Flinty916 <38919621+Flinty916@users.noreply.github.com>
2021-07-17 16:10:11 +0100

    Socialite tings

---
+++ web/app/Http/Controllers/Auth/SocialiteController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialiteController extends Controller
+{
+
+    private $redirect = '/';
+
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        $userSocial = Socialite::driver($provider)->stateless()->user();
+        $users = User::where(['email' => $userSocial->getEmail()])->first();
+        if ($users) {
+            Auth::login($users);
+            return redirect($this->redirect);
+        } else {
+            $user = User::create([
+                'name' => $userSocial->getName(),
+                'email' => $userSocial->getEmail(),
+                'image' => $userSocial->getAvatar(),
+                'provider_id' => $userSocial->getId(),
+                'provider' => $provider,
+            ]);
+            return redirect()->route('home');
+        }
+    }
+}
--- web/routes/web.php
+++ web/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Auth\SocialiteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('login/{provider}', [SocialiteController::class, '']);
+Route::get('login/{provider}/callback',[SocialiteController::class, '']);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
