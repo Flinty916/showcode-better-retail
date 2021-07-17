90db912cb5128df55a25e438955b9cead481be43
Luca Xue <lxue02@gmail.com>
2021-07-17 17:25:24 +0100

    Search Layout

--- web/resources/js/Pages/Search.tsx
+++ web/resources/js/Pages/Search.tsx
@@ -5,19 +5,24 @@ import Journey from "@/Components/Journey";
 
 export default function Search(props: React.PropsWithChildren<any>) {
     return (
-        <Authenticated
-            auth={props.auth}
-            header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Search
-                </h2>
-            }
-        >
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    {/* <ShoppingList /> */}
-                    <Journey />
-                </div>
+        <Authenticated auth={props.auth}>
+            <div className="bg-gradient-to-r from-indigo-400 to-indigo-500 h-screen">
+                <header className="grid gap-2 p-8">
+                    <h1 className="font-semibold text-white text-3xl">
+                        👋 Hi there!
+                    </h1>
+
+                    <label
+                        className="text-white font-semibold text-lg"
+                        htmlFor="items"
+                    >
+                        Search for items
+                    </label>
+                    <input className="rounded-lg py-2 shadow-lg" name="items" />
+                </header>
+
+                <main className="bg-white rounded-2xl h-full">
+                </main>
             </div>
         </Authenticated>
     );
