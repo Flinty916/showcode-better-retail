11bc46d68ee59dbd6782986bc7e659e0dc247734
Flinty916 <38919621+Flinty916@users.noreply.github.com>
2021-07-17 13:42:41 +0100

    Create/Update Node Requests

---
+++ web/app/Http/Requests/Node/CreateNodeRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Node;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateNodeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'type_id' => 'required|integer',
+            'coordX' => 'required|integer',
+            'coordY' => 'required|integer',
+            'sizeX' => 'required|integer',
+            'sizeY' => 'required|integer',
+        ];
+    }
+}
